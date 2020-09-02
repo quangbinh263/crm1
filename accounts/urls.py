0804e78acaa881811538640e34f18edc21d91f06
David Webb <quangbinhtrinh263@gmail.com>
2020-09-02 13:08:00 +0700

    second commit

---
+++ accounts/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('products/', views.products, name='products'),
+    path('customer/<str:pk>/', views.customer, name='customer'),
+    path('create_order/<str:pk>/', views.createOrder, name='create_order'),
+    path('update_order/<str:pk>/', views.updateOrder, name='update_order'),
+    path('delete_order/<str:pk>/', views.deleteOrder, name='delete_order'),
+    path('login/', views.loginPage, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('user/', views.userPage, name='user-page'),
+    path('account/', views.accountSettings, name='account'),
+]
+
+
